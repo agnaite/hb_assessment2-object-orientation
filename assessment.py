9eb3b0493b7bb6b1c80e56e010dbc4762e029234
Agnė Klimaitė <agnaite@me.com>
2016-07-16 16:41:07 -0700

    Added Quiz class

--- assessment.py
+++ assessment.py
@@ -12,7 +12,11 @@ class Student(object):
     def get_score(self, exam):
         """Returns a percentage score for the exam passed in"""
 
-        if self.score is not None:
+        if self.score == True:
+            return "{} passed the quiz.".format(self.first_name)
+        elif self.score == False:
+            return "{} failed the quiz.".format(self.first_name)
+        elif self.score is not None:
             return "{} scored: {:.2f}%.".format(self.first_name,
                                            (float(self.score)/len(exam.questions)) * 100)
         else:
@@ -60,6 +64,16 @@ class Exam(object):
 
         return score
 
+class Quiz(Exam):
+    """A quiz which inherits from Exam class"""
+
+    def administer(self):
+        score = super(Quiz, self).administer()
+        if score > float(len(self.questions))/2:
+            return True
+        else:
+            return False
+
 
 def take_test(exam, student):
     """Takes adminsters an exam to a student"""
@@ -78,6 +92,20 @@ def example():
     exam.add_question("Translate: 'What's for dinner?'", "Was gibst zum Abendbrot?")
 
     student = Student("Joanna", "Schmidt", "345 Florida Dr")
+
+    take_test(exam, student)
+    print student.get_score(exam)
+
+def quiz_example():
+    """Creates a student and a quiz"""
+
+    exam = Quiz('Addition 101')
+
+    exam.add_question("2 + 2", "4")
+    exam.add_question("1 + 4", "5")
+    exam.add_question("10 + 1", "11")
+
+    student = Student("Doron", "Fishman", "300 20th St")
 
     take_test(exam, student)
     print student.get_score(exam)
