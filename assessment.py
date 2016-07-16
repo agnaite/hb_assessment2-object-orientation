2f59cc529a70f5023560dd16e3c91bee3c66472c
Agnė Klimaitė <agnaite@me.com>
2016-07-16 15:52:56 -0700

    Made a student class

--- assessment.py
+++ assessment.py
@@ -1,23 +1,9 @@
-"""
-Part 1: Discussion
+class Student(object):
+    """A student"""
 
-1. What are the three main design advantages that object orientation
-   can provide? Explain each concept.
-   
-2. What is a class?
+    def __init__(self, first_name, last_name, address):
+        """A student has a first and last name and an address"""
 
-3. What is an instance attribute?
-
-4. What is a method?
-
-5. What is an instance in object orientation?
-
-6. How is a class attribute different than an instance attribute?
-   Give an example of when you might use each.
-
-
-"""
-
-
-# Parts 2 through 5:
-# Create your classes and class methods
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
